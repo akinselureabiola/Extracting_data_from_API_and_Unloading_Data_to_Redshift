8f9517cee7049f9c63d3cecadbad1174b133a100
Akinselure Abiola <akinselureabiola@Akinselures-MBP.fritz.box>
2024-02-03 14:58:47 +0100

    Updating readme

--- extract.py
+++ extract.py
@@ -5,9 +5,14 @@ import pandas as pd
 url = "http://universities.hipolabs.com/search?country=United+States"
 response = requests.get(url)
 data = response.text
+
 # converting string to JSON
 parsed = json.loads(data)
 json_data = print(json.dumps(parsed))
 new_df = pd.DataFrame({'col':parsed})
+
 # Convert DataFrame to Csv file format
-df.to_csv('new_df.csv', index=False)
+df_csv = df.to_csv('../test/uni.csv', index=False)
+
+#Convert CSV file to Paquet file format
+df_paquet = df.to_parquet('../test/uni.parquet', index=False)
